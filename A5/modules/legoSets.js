41f3d9d3a2b6a927aaee337f34e4686f47ac0ded
jadorotan <jadorotan@myseneca.ca>
2023-12-11 23:11:41 -0500

    editted legoSets.js

--- A5/modules/legoSets.js
+++ A5/modules/legoSets.js
@@ -5,7 +5,7 @@ const Sequelize = require('sequelize');
 // const themeData = require("../data/themeData");
 // let sets = [];
 
-const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASSWORD, {
+const sequelize = new Sequelize(process.env.DB_DATABASE,process.env.DB_USER,process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: 'postgres',
     port: 5432,
